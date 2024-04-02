6adaa67321fc87c8e8d3ec5c21ea8c38d5aa7332
BufferOverflow <scharxidev@outlook.de>
2024-04-02 16:20:42 +0200

    renamed 'controller' to 'handler'

--- routes/sample_route.go
+++ routes/sample_route.go
@@ -1,10 +1,10 @@
 package routes
 
 import (
-	"github.com/BxfferOverflow/go-gin-template/controller"
+	"github.com/BxfferOverflow/go-gin-template/handler"
 	"github.com/gin-gonic/gin"
 )
 
 func AddSampleRoutes(group *gin.RouterGroup) {
-	group.GET("/hello", controller.SayHello)
+	group.GET("/hello", handler.SayHello)
 }
